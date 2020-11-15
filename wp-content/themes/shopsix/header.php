b5bf1432fd07a06d5057b9d6ff05efe58aacc239
Long Nguyễn <vanlong20it@gmail.com>
2020-11-15 21:54:04 +0700

    Update site

--- wp-content/themes/shopsix/header.php
+++ wp-content/themes/shopsix/header.php
@@ -14,6 +14,9 @@
         <div class="container-fluid header-container clearfix">
             <div class="row">
                 <nav class="site-navigation col-xs-3 col-sm-3 col-md-3 col-lg-5">
+                    <span class="toggle-nav hidden-lg">
+                        <span class="icon-nav"></span>
+                    </span>
                     <ul id="menu-primary-menu" class="nav-menu">
                         <li class="menu-item current-menu">
                             <a href="#nav-item">Home</a>
@@ -33,11 +36,33 @@
                     </ul>
                 </nav>
                 <div class="site-branding col-xs-6 col-sn-5 col-md-6 col-lg-2">
-                    <a href="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" class="logo"></a>
+                    <a href="#home" class="logo">
+                        <span>Sh6p</span>
+                    </a>
+                    <h1 class="site-title"></h1>
+                    <p class="site-description"></p>
                 </div>
                 <div class="header-icon col-sx-3 col-sm-3 col-md-3 col-lg-5">
-
+                    <ul class="hidden-sm hidden-md hidden xs">
+                        <li class="menu-item menu-item-search">
+                            <a href="#">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li class="menu-item menu-item-account">
+                            <a href="#">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li class="menu-item menu-item-cart">
+                            <a href="#">
+                                <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>
+                                <span class="count cart-counter">0</span>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
     </header>
+    
